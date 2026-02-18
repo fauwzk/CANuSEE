f65b78670e2f1417c0ce2eabc6a86696b12725d2
Axel Polentes <polentesaxel@gmail.com>
2026-02-18 04:17:52 +0100

    Humm

--- extra_scripts/set_version.py
+++ extra_scripts/set_version.py
@@ -5,6 +5,5 @@ import os
 
 git_sha = os.getenv("GIT_SHA", "dev")
 print(f"Injecting FW_VERSION={git_sha}")
-env.Append(
-    BUILD_FLAGS=[f'-DFW_VERSION="{git_sha}"']
-)
+os.popen("sleep 2")  # Simulate a pause of 2 seconds
+env.Append(BUILD_FLAGS=[f'-DFW_VERSION="{git_sha}"'])
