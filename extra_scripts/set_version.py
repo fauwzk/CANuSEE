4ca2206081fc32c53bc269deec8f053ebe39e900
Axel Polentes <polentesaxel@gmail.com>
2026-02-18 04:22:10 +0100

    a

--- extra_scripts/set_version.py
+++ extra_scripts/set_version.py
@@ -5,5 +5,5 @@ import os
 
 git_sha = os.getenv("GIT_SHA", "dev")
 print(f"Injecting FW_VERSION={git_sha}")
-os.popen("sleep 2")  # Simulate a pause of 2 seconds
+print("Current BUILD_FLAGS:", env.get("BUILD_FLAGS", []))
 env.Append(BUILD_FLAGS=[f'-DFW_VERSION="{git_sha}"'])
