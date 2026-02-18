396f78bf0f5f6b950d3ce432f7544d6081b409c4
Axel Polentes <polentesaxel@gmail.com>
2026-02-18 04:24:08 +0100

    b

--- extra_scripts/set_version.py
+++ extra_scripts/set_version.py
@@ -1,9 +1,14 @@
 # extra_scripts/set_version.py
-
 Import("env")
 import os
 
 git_sha = os.getenv("GIT_SHA", "dev")
 print(f"Injecting FW_VERSION={git_sha}")
-print("Current BUILD_FLAGS:", env.get("BUILD_FLAGS", []))
-env.Append(BUILD_FLAGS=[f'-DFW_VERSION="{git_sha}"'])
+
+# Append to CPPDEFINES so it's visible in Arduino code
+env.Append(
+    CPPDEFINES=[("FW_VERSION", f'"{git_sha}"')]
+)
+
+# Optional: debug output
+print("Current CPPDEFINES:", env.get("CPPDEFINES", []))
